32b1e7d556da8197a7352ea1f5440bb36d3ee43a
FranciscoJaviev <franciscojaviev@outlook.it>
2025-05-14 14:22:33 +0200

    Update Star Classification AlberoDecisionale.py

--- Star Classification AlberoDecisionale.py
+++ Star Classification AlberoDecisionale.py
@@ -26,18 +26,18 @@ class_name = ['Galassia', 'Stella', 'QSO']
 feature_names=["alpha","delta","u","g","r","i","z"]
 
 
-# 2. Divisione dei Dati in Training e Test
+# Divisione dei Dati in Training e Test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# 3. Creazione e Addestramento del Modello dell'Albero Decisionale
+# Creazione e Addestramento del Modello dell'Albero Decisionale
 # Puoi sperimentare con diversi criteri ('gini' o 'entropy') e profondità massima
 albero_decisionale = DecisionTreeClassifier(criterion='gini', max_depth=3, random_state=42)
 albero_decisionale.fit(X_train, y_train)
 
-# 4. Previsioni sul Set di Test
+# Previsioni sul Set di Test
 y_pred = albero_decisionale.predict(X_test)
 
-# 5. Valutazione del Modello
+# Valutazione del Modello
 print("Risultati dell'Albero Decisionale:")
 print("\nMatrice di Confusione:")
 cm = confusion_matrix(y_test, y_pred)
@@ -51,7 +51,7 @@ plt.show()
 print("\nClassification Report:")
 print(classification_report(y_test, y_pred, target_names=class_name, zero_division=1))
 
-# 6. (Opzionale) Visualizzazione dell'Albero Decisionale
+# (Opzionale) Visualizzazione dell'Albero Decisionale
 plt.figure(figsize=(10, 8))
 tree.plot_tree(albero_decisionale, feature_names=feature_names, class_names=class_name, filled=True)
 plt.title("Albero Decisionale Addestrato")
